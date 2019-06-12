2236a8ad9602e4d47c15fc6d530283ddbd206591
renugambal <51654383+renugambal@users.noreply.github.com>
2019-06-12 12:15:34 +0530

    Create vowels or consonant.py

---
+++ codekata/vowels or consonant.py
@@ -0,0 +1,9 @@
+a=input()
+l=['a','e','i','o','u']
+list=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+if a in l:
+  print("Vowel")
+elif a in list:
+  print("Consonant")
+else:
+  print("invalid")  
