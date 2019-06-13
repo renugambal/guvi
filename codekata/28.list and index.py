58f6bb1d4ff65056b82a35bbe8a8cbc8ea781dd0
renugambal <51654383+renugambal@users.noreply.github.com>
2019-06-13 16:00:45 +0530

    Update 28.list and index.py

--- codekata/28.list and index.py
+++ codekata/28.list and index.py
@@ -1,6 +1,5 @@
-a=int(input())
+s=int(input())
 lis=list(map(int,input().split()))
-if len(lis)==a:
-  for i in range(a):
-    print(lis[i]," ",i)
-  
+if len(lis)==s:
+  for i in range(s):
+    print(lis[i],i)
