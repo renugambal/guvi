c7f2ab0496af5230c095ebc691565f6dbe90eebc
renugambal <51654383+renugambal@users.noreply.github.com>
2019-06-14 13:55:22 +0530

    Create 52.print number.py

---
+++ codekata/52.print number.py
@@ -0,0 +1,21 @@
+h=int(input())
+if h==1:
+  print("One")
+elif h==2:
+  print("Two")
+elif h==3:
+  print("Three")
+elif h==4:
+  print("Four")
+elif h==5:
+  print("Five")
+elif h==6:
+  print("Six")
+elif h==7:
+  print("Seven")
+elif h==8:
+  print("Eight")
+elif h==9:
+  print("Nine")
+elif h==10:
+  print("Ten")
