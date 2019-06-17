c054a7f09054df0f45536997281db7dea8acffb5
renugambal <51654383+renugambal@users.noreply.github.com>
2019-06-17 19:47:58 +0530

    Create 67.multiple of ten.py

---
+++ codekata/67.multiple of ten.py
@@ -0,0 +1,22 @@
+a=int(input())
+b=a
+if a%10==1:
+    print(b+9)
+elif a%10==2:
+    print(b+8)
+elif a%10==3:
+    print(b+7)
+elif a%10==4:
+    print(b+6)
+elif a%10==5:
+    print(b+5)
+elif a%10==6:
+    print(b+4)
+elif a%10==7:
+    print(b+3)
+elif a%10==8:
+    print(b+2)
+elif a%10==9:
+    print(b+1)
+else:
+    print(b)
